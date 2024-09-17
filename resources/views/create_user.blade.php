c53f54774506ffb9031da5ddf3bf24eba615e0f7
rafrmdhn <rafiramadhanarr17@gmail.com>
2024-09-17 14:29:48 +0700

    Add Model, Migration, and Seeders

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -3,18 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulir Pembuatan User</title>
 </head>
 <body>
-    <h1>Ini Halaman User</h1>
+    <h1>Buat User Baru</h1>
 
     <form action="{{ route('user.store') }}" method="post">
         @csrf
-        <label for="nama">First name:</label><br>
-        <input type="text" id="nama" name="nama"><br>
-        <label for="npm">Last name:</label><br>
-        <input type="text" id="npm" name="npm"><br><br>
+        <label for="nama">Nama:</label><br>
+        <input type="text" id="nama" name="nama" required><br>
+        
+        <label for="npm">NPM:</label><br>
+        <input type="text" id="npm" name="npm" required><br>
+        
+        <label for="id_kelas">Kelas:</label><br>
+        <select name="kelas_id" id="kelas_id" required>
+            @foreach ($kelas as $kelasItem)
+                <option value="{{ $kelasItem->id }}">{{ $kelasItem->nama_kelas }}</option>
+            @endforeach
+        </select><br><br>
+        
         <input type="submit" value="Submit">
-    </form> 
+    </form>
 </body>
 </html>
